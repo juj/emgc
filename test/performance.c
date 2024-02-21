1b4b2416ef349ee54fc12a33a39a218a8d0ee5c7
Jukka Jylänki <jujjyl@gmail.com>
2024-02-21 22:52:43 +0200

    Add performance test.

---
+++ test/performance.c
@@ -0,0 +1,17 @@
+#include "test.h"
+#include <emscripten/html5.h>
+
+int main()
+{
+  srand(emscripten_random()*(2048u*1024*1024));
+  uint32_t size = 3800ull*1024*1024;
+  uint32_t *large = gc_malloc(size);
+  require(large != 0);
+  for(uint32_t i = 0; i < 900ull*1024*1024; ++i)
+    if ((rand() % 10) < 3)
+      large[i] = rand();
+  double t0 = emscripten_performance_now();
+  gc_collect(); // will not scan contents of 'string', so the second allocation should be collected.
+  double t1 = emscripten_performance_now();
+  printf("gc_collect() took %f msecs. i.e. marked %f MB/second.\n", t1-t0, size * 1000.0 / ((t1-t0)*1024*1024));
+}
