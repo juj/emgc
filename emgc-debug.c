a565159c08c7e82be756a99db4ce1eef1679eac3
Jukka Jylänki <jujjyl@gmail.com>
2024-02-23 16:20:38 +0200

    Fix gc_dump() when table is zero sized.

--- emgc-debug.c
+++ emgc-debug.c
@@ -5,7 +5,8 @@ uint32_t gc_num_ptrs()
 
 void gc_dump()
 {
-  for(uint32_t i = 0; i <= table_mask; ++i)
-    if (table[i] > SENTINEL_PTR) EM_ASM({console.log(`Table index ${$0}: 0x${$1.toString(16)}`);}, i, table[i]);
+  if (table)
+    for(uint32_t i = 0; i <= table_mask; ++i)
+      if (table[i] > SENTINEL_PTR) EM_ASM({console.log(`Table index ${$0}: 0x${$1.toString(16)}`);}, i, table[i]);
   EM_ASM({console.log(`${$0} allocations total, ${$1} used table entries. Table size: ${$2}`);}, num_allocs, num_table_entries, table_mask+1);
 }
