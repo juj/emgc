7047cdb941722c419fc02260de705a2ee0ed3497
Jukka Jylänki <jujjyl@gmail.com>
2024-02-23 11:07:31 +0200

    Add test for alloca() behavior.

---
+++ test/stack_alloca.c
@@ -0,0 +1,19 @@
+// Tests garbage collection from GC pointers stored in an alloca().
+#include "test.h"
+
+void func()
+{
+  char **stack = (char**)alloca(4);
+  stack[0] = (char*)gc_malloc(1024);
+  gc_collect();
+  require(gc_num_ptrs() == 1 && "Managed pointer on stack should not have gotten garbage collected, but it did.");
+  PIN(stack); // Use pointer on stack so that it is alive across the garbage collect call above.
+}
+
+int main()
+{
+  CALL_INDIRECTLY(func);
+
+  gc_collect();
+  require(gc_num_ptrs() == 0 && "Pointer on stack in an old called function should have gotten freed, but didn't.");
+}
