ce9f635024aaef1d2f02d2da8a6617e3b889c763
Jukka Jylänki <jujjyl@gmail.com>
2024-02-24 00:38:42 +0200

    Fix SIMD marking.

--- emgc-simd.c
+++ emgc-simd.c
@@ -23,17 +23,17 @@ static void mark(void *ptr, size_t bytes)
     while(bits)
     {
       int offset = __builtin_ctz(bits);
-      bits = (bits >> offset) ^ 1;
+      bits ^= 1 << offset;
       void *ptr = p[offset];
       for(uint32_t i = hash_ptr(ptr); table[i]; i = (i+1) & table_mask)
         if (REMOVE_FLAG_BITS(table[i]) == ptr)
         {
           if (i != INVALID_INDEX && !BITVEC_GET(mark_table, i))
           {
-//            EM_ASM({console.log(`Marked ptr ${$0.toString(16)} at index ${$1} from memory address ${$2.toString(16)}.`)}, *p, i, p);
+//            EM_ASM({console.log(`Marked ptr ${$0.toString(16)} at index ${$1} from memory address ${$2.toString(16)}.`)}, ptr, i, p+offset);
             BITVEC_SET(mark_table, i);
             num_finalizers_marked += HAS_FINALIZER_BIT(table[i]);
-            if (!HAS_LEAF_BIT(table[i])) mark(*p, malloc_usable_size(*p));
+            if (!HAS_LEAF_BIT(table[i])) mark(ptr, malloc_usable_size(ptr));
           }
           break;
         }
