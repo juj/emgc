8e9038882766ee172ec8b56a9ceeab88b406924e
Jukka Jylänki <jujjyl@gmail.com>
2024-02-23 10:59:56 +0200

    Move debugging functionality into emgc-debug.c.

--- emgc.c
+++ emgc.c
@@ -191,14 +191,4 @@ void gc_collect_when_stack_is_empty()
   emscripten_set_timeout(collect_when_stack_is_empty, 0, 0);
 }
 
-uint32_t gc_num_ptrs()
-{
-  return num_allocs;
-}
-
-void gc_dump()
-{
-  for(uint32_t i = 0; i <= table_mask; ++i)
-    if (table[i] > SENTINEL_PTR) EM_ASM({console.log(`Table index ${$0}: 0x${$1.toString(16)}`);}, i, table[i]);
-  EM_ASM({console.log(`${$0} allocations total, ${$1} used table entries. Table size: ${$2}`);}, num_allocs, num_table_entries, table_mask+1);
-}
+#include "emgc-debug.c"
