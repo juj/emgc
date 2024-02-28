a57ee3472e325a4a44d8542f282694a7a26a68a8
Jukka Jylänki <jujjyl@gmail.com>
2024-02-28 19:23:19 +0200

    Fix num_allocs calculation.

--- emgc.c
+++ emgc.c
@@ -83,7 +83,7 @@ static void realloc_table()
   used_table = (uint8_t*)calloc(((table_mask+1)>>3)
                                + (table_mask+1)*sizeof(void*), sizeof(uint8_t));
   table = (void**)(used_table + ((table_mask+1)>>3));
-  num_table_entries = 0;
+  num_table_entries = num_allocs = 0;
 
   if (old_table)
     for(uint32_t i = 0, offset; i <= old_mask; i += 64)
