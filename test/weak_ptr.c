0532ff46f56fac4d3d862cabfa808a842597670a
Jukka Jylänki <jujjyl@gmail.com>
2026-03-04 19:49:36 +0200

    Update weak_ptr to pass after updated weak ptr semantics

--- test/weak_ptr.c
+++ test/weak_ptr.c
@@ -9,10 +9,10 @@ void func()
   global = (void*)gc_get_weak_ptr(gc_malloc(1024));
   PIN(&global);
   require(global && "Alloc must have succeeded");
-  require(gc_num_ptrs() == 1);
+  require(gc_num_ptrs() == 2); // We should have two allocs: one for the strong ptr, and another for the control block of the weak ptr.
 
   require(gc_is_weak_ptr(global) && "gc_get_weak_ptr() should have returned a weak pointer.");
-  void *strong = gc_acquire_strong_ptr(global);
+  void *strong = gc_acquire_strong_ptr(&global);
   require(strong != 0 && "gc_acquire_strong_ptr() should have worked before GC.");
   require(gc_is_strong_ptr(strong) && "gc_acquire_strong_ptr() should have returned a strong pointer.");
 }
@@ -22,8 +22,8 @@ int main()
   CALL_INDIRECTLY(func);
 
   gc_collect();
-  require(gc_num_ptrs() == 0 && "A weak pointer should not have prevented GC.");
+  require(gc_num_ptrs() == 1 && "A weak pointer should not have prevented GC.");
 
-  void *strong = gc_acquire_strong_ptr(global);
+  void *strong = gc_acquire_strong_ptr(&global);
   require(strong == 0 && "Acquiring strong pointer from stale weak pointer should no longer work.");
 }
