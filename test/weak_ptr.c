d7c4f67561110338142141067ac4ac637f481448
Jukka Jylänki <jujjyl@gmail.com>
2026-03-05 23:44:05 +0200

    Clarify weak_ptr.c test a bit.

--- test/weak_ptr.c
+++ test/weak_ptr.c
@@ -6,15 +6,18 @@ void *global;
 
 void func()
 {
-  global = (void*)gc_get_weak_ptr(gc_malloc(1024));
+  void *ptr = gc_malloc(1024);
+  global = (void*)gc_get_weak_ptr(ptr);
   PIN(&global);
   require(global && "Alloc must have succeeded");
   require(gc_num_ptrs() == 2); // We should have two allocs: one for the strong ptr, and another for the control block of the weak ptr.
 
   require(gc_is_weak_ptr(global) && "gc_get_weak_ptr() should have returned a weak pointer.");
   void *strong = gc_acquire_strong_ptr(&global);
-  require(strong != 0 && "gc_acquire_strong_ptr() should have worked before GC.");
-  require(gc_is_strong_ptr(strong) && "gc_acquire_strong_ptr() should have returned a strong pointer.");
+  require(strong != 0 && "gc_acquire_strong_ptr() should not return null.");
+  require(strong == ptr && "gc_acquire_strong_ptr() should have required the original allocated pointer.");
+
+  require(gc_is_strong_ptr(strong) && "gc_acquire_strong_ptr() should detect correctly as a gc_is_strong_ptr().");
 }
 
 int main()
