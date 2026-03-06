7cc950480798c3ea3ba529e02d828b770ab9ad8f
Jukka Jylänki <jujjyl@gmail.com>
2026-03-06 16:28:04 +0200

    Rewrite gc_ptr_base() to do a full scan of managed allocations. This is slow, but works as a reference implementation until a better one is available.

--- emgc-ptr_base.c
+++ emgc-ptr_base.c
@@ -7,16 +7,18 @@ void *gc_ptr_base(void *ptr)
   if ((uintptr_t)ptr - (uintptr_t)&__heap_base >= (uintptr_t)emscripten_get_heap_size() - (uintptr_t)&__heap_base) return 0;
   if (!num_allocs) return 0;
 
-  uint32_t i = hash_ptr(ptr);
-  if (!table[i]) while(!table[i]) i = (i + table_mask) & table_mask;
-  else           while(table[(i+1) & table_mask]) i = (i+1) & table_mask;
-
-  void *max = 0;
-  for(; table[i]; --i)
-    if (table[i] != SENTINEL_PTR)
+  // TODO: Rewrite this to utilize an allocation range interval tree.
+  GC_MALLOC_ACQUIRE();
+  for(uint32_t i = 0, offset; i <= table_mask; i += 64)
+    for(uint64_t bits = used_table[i>>6]; bits; bits ^= (1ull<<offset))
     {
-      void *t = REMOVE_FLAG_BITS(table[i]);
-      if (t <= ptr && t > max) max = t;
+      void *managed_ptr = REMOVE_FLAG_BITS(table[i + (offset = __builtin_ctzll(bits))]);
+      if (ptr >= managed_ptr && (uintptr_t)ptr - (uintptr_t)managed_ptr < malloc_usable_size(managed_ptr))
+      {
+        GC_MALLOC_RELEASE();
+        return managed_ptr;
+      }
     }
-  return ((uintptr_t)ptr <= (uintptr_t)max + malloc_usable_size(max)) ? max : 0;
+  GC_MALLOC_RELEASE();
+  return 0;
 }
