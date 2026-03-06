56f14ffb8b3910138653a5591e5f1b4fb526b657
Jukka Jylänki <jujjyl@gmail.com>
2026-03-06 16:49:05 +0200

    Adjust comment on gc_ptr_base()

--- emgc-ptr_base.c
+++ emgc-ptr_base.c
@@ -7,7 +7,8 @@ void *gc_ptr_base(void *ptr)
   if ((uintptr_t)ptr - (uintptr_t)&__heap_base >= (uintptr_t)emscripten_get_heap_size() - (uintptr_t)&__heap_base) return 0;
   if (!num_allocs) return 0;
 
-  // TODO: Rewrite this to utilize an allocation range interval tree.
+  // This is intentionally a O(n) scan over the whole managed alloc table for now.
+  // TODO: Rewrite this to utilize an allocation range interval tree for performance.
   GC_MALLOC_ACQUIRE();
   for(uint32_t i = 0, offset; i <= table_mask; i += 64)
     for(uint64_t bits = ((uint64_t*)used_table)[i>>6]; bits; bits ^= (1ull<<offset))
